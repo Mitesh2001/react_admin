af9641a40163243b9ff679b43fafb6248ac3c358
yashbuhecha <yashbuhecha1998@gmail.com>
2023-09-06 10:19:38 +0530

    menu & nav created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Menu from './components/Menu';
+import Nav from './components/Nav';
+import Users from './pages/Users';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' index element={<Dashboard />} />
+          <Route path='/users' element={<Users />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,15 @@
+import { Component, ReactNode } from "react";
+import Wrapper from "../components/Wrapper";
+
+class Dashboard extends Component {
+    render(): ReactNode {
+        return (
+            <Wrapper>
+                <div>
+                    Dashboard
+                </div>
+            </Wrapper>
+        );
+    }
+}
+export default Dashboard;
